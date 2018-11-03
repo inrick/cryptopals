4b64aa6f25a314fdf0bee3c75b916d452b40745d
Fabian Carlström <git@inrick.se>
2018-11-03 21:04:57 +0100

    Move aes to separate package & move 1.7 to test

--- main_test.go
+++ main_test.go
@@ -3,6 +3,7 @@ package main
 import (
 	"bufio"
 	"bytes"
+	"cryptopals/aes"
 	"io/ioutil"
 	"math/rand"
 	"os"
@@ -204,60 +205,23 @@ func TestChallenge1_6(t *testing.T) {
 	}
 }
 
-func TestShiftRows(t *testing.T) {
-	tests := []struct{ input, want []byte }{{
-		[]byte{0, 4, 8, 12, 1, 5, 9, 13, 2, 6, 10, 14, 3, 7, 11, 15},
-		[]byte{0, 5, 10, 15, 1, 6, 11, 12, 2, 7, 8, 13, 3, 4, 9, 14},
-	}, {
-		[]byte{0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15},
-		[]byte{0, 5, 10, 15, 4, 9, 14, 3, 8, 13, 2, 7, 12, 1, 6, 11},
-	}}
-
-	for _, test := range tests {
-		input, want := test.input, test.want
-		shiftRows(input)
-		if !bytes.Equal(input, want) {
-			t.Errorf("ShiftRows failed: got %v, want %v", input, want)
-		}
+func TestChallenge1_7(t *testing.T) {
+	input, err := readBase64File("data/7.txt")
+	if err != nil {
+		panic(err)
 	}
-}
 
-func TestShiftRowsInv(t *testing.T) {
-	input := []byte{0, 4, 8, 12, 1, 5, 9, 13, 2, 6, 10, 14, 3, 7, 11, 15}
-	want := []byte{0, 7, 10, 13, 1, 4, 11, 14, 2, 5, 8, 15, 3, 6, 9, 12}
-
-	shiftRowsInv(input)
-	if !bytes.Equal(input, want) {
-		t.Errorf("ShiftRowsInv failed: got %v, want %v", input, want)
-	}
-}
-
-func TestMixColumns(t *testing.T) {
-	//input := []byte{0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15}
-	//want := []byte{0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15}
-	for i := 0; i < 128; i++ {
-		input := make([]byte, 16)
-		want := make([]byte, 16)
-		rand.Read(input)
-		copy(want, input)
-
-		mixColumns(input) // = u
-		if bytes.Equal(want, input) {
-			t.Errorf("MixColumns failed: MixColumns(%v) didn't do anything", input)
-		}
-		mixColumnsInv(input) // = got
-		if !bytes.Equal(want, input) {
-			t.Errorf("MixColumns failed: wanted MixColumnsInv(MixColumns(%v)) = %v, got %v", want, want, input)
-		}
-	}
-}
-
-func TestAes128(t *testing.T) {
 	key := []byte("YELLOW SUBMARINE")
-	txt := []byte("I'm back and I'm")
-	enc := aes128EncryptBlock(key, txt)
-	dec := aes128DecryptBlock(key, enc)
-	if !bytes.Equal(dec, txt) {
-		t.Errorf("AES128 failed: key=%d, enc=%d, dec=%d", key, enc, dec)
+	got, err := aes.EcbDecrypt128(key, input)
+	if err != nil {
+		panic(err)
+	}
+
+	want, err := ioutil.ReadFile("data/want1_7.txt")
+	if err != nil {
+		t.Fatal(err)
+	}
+	if !bytes.Equal(got, want) {
+		t.Errorf("Challenge 1.7 failed: got\n'%v'\nwant\n'%v'", got, want)
 	}
 }
